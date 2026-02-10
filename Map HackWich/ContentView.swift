859be59101f75115c3c22aa5945618b9254f4a49
Gabby <gabriellelaah@gmail.com>
2026-02-10 10:19:55 -0600

    Initial Commit

---
+++ Map HackWich/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Map HackWich
+//
+//  Created by gabrielle on 2/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Map HackWich/Map_HackWichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Map_HackWichApp.swift
+//  Map HackWich
+//
+//  Created by gabrielle on 2/10/26.
+//
+
+import SwiftUI
+
+@main
+struct Map_HackWichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
